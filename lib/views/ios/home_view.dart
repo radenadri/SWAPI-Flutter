045c291a069806387abe6e1a96801a0c19c82d88
Adriana Eka Prayudha <radenadriep@gmail.com>
2024-08-19 15:02:27 +0700

    fix: only people and settings page

--- lib/views/ios/home_view.dart
+++ lib/views/ios/home_view.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:swapi_app/views/ios/explore_view.dart';
-import 'package:swapi_app/views/ios/film_view.dart';
 import 'package:swapi_app/views/ios/people_view.dart';
-import 'package:swapi_app/views/ios/planet_view.dart';
 import 'package:swapi_app/views/ios/setting_view.dart';
 
 class HomeView extends StatefulWidget {
@@ -19,20 +16,8 @@ class _HomeViewState extends State<HomeView> {
       tabBar: CupertinoTabBar(
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.rectangle_3_offgrid),
-            label: 'Explore',
-          ),
-          BottomNavigationBarItem(
             icon: Icon(CupertinoIcons.person_3),
             label: 'People',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.film),
-            label: 'Films',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.globe),
-            label: 'Planets',
           ),
           BottomNavigationBarItem(
             icon: Icon(CupertinoIcons.gear),
@@ -44,16 +29,10 @@ class _HomeViewState extends State<HomeView> {
         return CupertinoTabView(
           builder: (context) {
             switch (index) {
-              case 1:
+              case 0:
                 return const PeopleView();
-              case 2:
-                return const FilmView();
-              case 3:
-                return const PlanetView();
-              case 4:
-                return const SettingView();
               default:
-                return const ExploreView();
+                return const SettingView();
             }
           },
         );
